f30d924457fd1ba9d067ce6742e7d71e0a3fce03
rakellbandeira <per22016l@byui.edu>
2025-03-27 11:41:18 -0400

    W03 Learning Activities: Content Delivery and the MVC Pattern

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express()
 const static = require("./routes/static")
 const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
+const utilities = require("./utilities/")
 
 /* ***********************
  * View Engine and Templates
@@ -30,10 +31,34 @@ app.use(static)
 /* app.get("/", function(req, res) {
   res.render("index", {title: "Home"})
 }) */
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+
+// File Not Found Route 
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
+
 
 
 /* ***********************
