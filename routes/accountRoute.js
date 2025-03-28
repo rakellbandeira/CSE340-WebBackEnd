3d091c686bd4368a374ce15a4905ca396a6aefd3
rakellbandeira <per22016l@byui.edu>
2025-03-27 20:39:31 -0400

    Part2 activities

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,8 @@ const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
+
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
@@ -11,6 +13,11 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
 // Processing the registration data
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
-
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
 module.exports = router
